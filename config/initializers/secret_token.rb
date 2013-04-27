a578f492a0544b50f1053ca4c0482381fdce54ce
Juan Figuera <juanfiguera@gmail.com>
2013-04-27 19:44:28 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Klastarter::Application.config.secret_token = '8a749e1b3a7b959ce1fc5aa6ef9b554ae6a39b794780438799e17f9b8663a1afd7c450018a0198f121c776be9e05ffb74c91f6441684a7bd61edec4b1d295707'
